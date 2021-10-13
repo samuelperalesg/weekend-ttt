3f25afe2e0aa70ab7c31f7899297cb0889637042
s.pera6@gmail.com <s.pera6@gmail.com>
2021-10-12 20:36:03 -0600

    add render & handleClick functions

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const winningCombos = [
   [2,5,8],
   [0,4,9],
   [2,4,6]
-];
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -19,7 +19,7 @@ let isWinner = null
 
 
 /*------------------------ Cached Element References ------------------------*/
-const squaresEl = [...document.querySelectorAll('.square')]
+const squaresEl = document.querySelectorAll('.square')
 const messageEl = document.querySelector('#message')
 const resetBtn = document.getElementById('reset')
 /*----------------------------- Event Listeners -----------------------------*/
@@ -34,7 +34,7 @@ resetBtn.addEventListener('click', init)
 init ()
 
 function init(){
-  messageEl.innerText = 'Player 1, pick a square!'
+  // messageEl.innerText = 'Player 1, pick a square!'
   squaresEl.innerHTML = ''
   boardState = [null, null, null, null, null, null, null, null, null]
   isWinner = null
@@ -47,6 +47,34 @@ function init(){
 
 function render(){
   boardState.forEach((square, idx) => {
-    square.innerText = board[idx]
+    let squareColor
+    let squareLetter
+    if (square === 1) {
+      squareColor = 'orange'
+      squareLetter = 'X'
+    } else if (square = -1) {
+      squareColor = 'green'
+      squareLetter = 'O'
+    } else if (square = null) {
+      squareColor = 'white'
+      squareLetter = ''
+    }
+    squaresEl[idx].style.background = squareColor
+    squaresEl[idx].style.background= squareLetter 
   })
 }
+
+
+
+function handleClick(evt){
+  let squareIdx = parseInt(evt.target.id.replace('sq', ''))
+}
+
+if (boardState[squareIdx] || isWinner) {
+  return 
+}
+
+boardState[squareIdx] = playerTurn
+playerTurn *= -1
+
+isWinner = getWinner()
